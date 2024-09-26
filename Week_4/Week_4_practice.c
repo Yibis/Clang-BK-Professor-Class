9b7146153456ee6fa54bd797395d6a272ad33187
Yibis <121001868+Yibis@users.noreply.github.com>
2024-09-26 11:17:30 +0900

    4주차 실습수업 문제

---
+++ Week_4/Week_4_practice.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+#define GOODS 5
+
+void stockGoods(int* stock);
+void saleGoods(int* sale);
+void inputGoodsName(char name[][20]);
+void statusGoods(int* stock, int* sale, char name[][20]);
+
+int main() {
+	int stock[GOODS] = { 0 };
+	int sale[GOODS] = { 0 };
+	char name[GOODS][20] = { 0 };
+	int num;
+
+
+	do {
+		printf("원하는 메뉴를 선택하세요.(1. 입고, 2. 판매, 3. 상품현황, 4. 상품명 입력, 5. 종료)\n");
+		scanf_s("%d", &num);
+		getchar();
+		printf("\n");
+
+		switch (num) {
+		case 1:
+			stockGoods(stock);
+			break;
+		case 2:
+			saleGoods(sale);
+			break;
+		case 3:
+			statusGoods(stock, sale, name);
+			break;
+		case 4:
+			inputGoodsName(name);
+			break;
+		case 5:
+			printf("프로그램을 종료합니다.\n");
+			break;
+		default:
+			printf("다시 입력해주세요.\n");
+			break;
+		}
+		printf("\n");
+	} while (num != 5);
+}
+
+
+void stockGoods(int* stock) {
+	int num, id, temp;
+
+	printf("입고수량 입력 : 전체 상품 입고수량 입력 1, 개별 상품 입력 2를 선택\n1 2 선택 : ");
+	scanf_s("%d", &num);
+
+	if (num == 1) {
+		printf("상품 별 입고수량 입력 : ");
+		for (int i = 0; i < GOODS; i++) {
+			scanf_s("%d", &temp);
+			stock[i] += temp;
+		}
+	}
+	else if (num == 2) {
+		printf("상품ID : ");
+		scanf_s("%d", &id);
+		if (id <= GOODS && id >= 1) {
+			printf("입고수량 입력 : ");
+			scanf_s("%d", &temp);
+			stock[id - 1] += temp;
+		}
+		else {
+			printf("잘못된 입력입니다.");
+		}
+	}
+	else {
+		printf("잘못된 입력입니다.");
+	}
+
+}
+
+void saleGoods(int* sale) {
+	int num, id, temp;
+
+	printf("판매수량 입력 : 전체 상품 판매수량 입력 1, 개별 상품 입력 2를 선택\n1 2 선택 : ");
+	scanf_s("%d", &num);
+
+	if (num == 1) {
+		printf("상품 별 판매수량 입력 : ");
+		for (int i = 0; i < GOODS; i++) {
+			scanf_s("%d", &temp);
+			sale[i] += temp;
+		}
+	}
+	else if (num == 2) {
+		printf("상품ID : ");
+		scanf_s("%d", &id);
+		if (id <= GOODS && id >= 1) {
+			printf("판매수량 입력 : ");
+			scanf_s("%d", &temp);
+			sale[id - 1] += temp;
+		}
+		else {
+			printf("잘못된 입력입니다.");
+		}
+	}
+	else {
+		printf("잘못된 입력입니다.");
+	}
+}
+
+void inputGoodsName(char name[][20]) {
+	for (int i = 0; i < GOODS; i++)
+	{
+		printf("ID %d 상품명 : ", i + 1);
+		gets_s(name[i], 20);
+	}
+}
+
+void statusGoods(int* stock, int* sale, char name[][20]) {
+	int totalStock = 0, totalSale = 0, minSale = 0, maxSale = 0;
+	int temp;
+
+	for (int i = 0; i < GOODS; i++) {
+		totalSale += sale[i];
+		totalStock += stock[i];
+
+		if (sale[minSale] > sale[i]) minSale = i;
+		if (sale[maxSale] < sale[i]) maxSale = i;
+	}
+
+	printf("재고수량 : ");
+	for (int i = 0; i < GOODS; i++) {
+		printf("%d ", stock[i] - sale[i]);
+	}
+
+	double saleRatio = ((double)totalSale / totalStock) * 100;
+	printf("\n총 판매량 : %d (판매율: %.2f%%)\n", totalSale, saleRatio);
+
+	printf("가장 많이 판매된 상품: ID %d, 상품명: %s, 판매량 %d\n", maxSale + 1, name[maxSale], sale[maxSale]);
+	printf("가장 적게 판매된 상품: ID %d, 상품명: %s, 판매량 %d\n", minSale + 1, name[minSale], sale[minSale]);
+
+	for (int i = 0; i < GOODS; i++) {
+		if (stock[i] - sale[i] <= 2) {
+			printf("상품 ID %d: 상품명 : %s, 재고부족(%d)\n", i + 1, name[i], stock[i] - sale[i]);
+		}
+	}
+}
