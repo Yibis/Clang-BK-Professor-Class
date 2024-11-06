7cf1b2028e83f00ad7313a9660d280e39a5990b1
Yibis <121001868+Yibis@users.noreply.github.com>
2024-11-06 22:42:10 +0900

    9주차3.FILE 3. 텍스트실습1_1650

---
+++ Week_9/file_processing_1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int getline(char* line);
+
+int main() {
+	char input[100];
+	FILE* fp;
+
+	if ((fp = fopen("output.txt", "w")) == NULL) {
+		printf("error...");
+		return 0;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		getline(input);
+		fputs(input, fp);
+		fputs("\n", fp);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+int getline(char* line) {
+	int ch;
+	int i = 0;
+
+	while ((ch = getchar()) != '\n') line[i++] = ch;
+	line[i] = '\0';
+
+	return i;
+}
