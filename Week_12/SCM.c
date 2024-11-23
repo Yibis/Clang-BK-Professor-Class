dd38045e63cd23f99f6e7ad4436aaac695da5197
Yibis <121001868+Yibis@users.noreply.github.com>
2024-11-23 13:48:47 +0900

    12주차_Practice2.소스코드관리(1133)

---
+++ Week_12/SCM.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int main() {
+    //printf("%d", add(10, 20));
+    //printf("%d", sub(10, 20));
+    //printf("%d", multi(10, 20));
+    printf("%d", printAdd(10, 20));
+    return 0;
+}
